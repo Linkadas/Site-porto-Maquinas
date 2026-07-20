1642b5fdf636ef498d80308c06dbf37187dd2b7a
Lincoln Asaga <lincolnshigueo@hotmail.com>
2026-07-20 19:28:11 -0300

    style: alinhar painel de representantes a direita

--- force_cache.py
+++ force_cache.py
@@ -8,12 +8,12 @@ for file in html_files:
     with open(file, 'r', encoding='utf-8') as f:
         content = f.read()
 
-    # Append ?v=24 to any .css that does not already have a query parameter
-    # Example: href="./css/index.css" -> href="./css/index.css?v=24"
-    new_content = re.sub(r'\.css(["\'])', r'.css?v=24\1', content)
+    # Append ?v=25 to any .css that does not already have a query parameter
+    # Example: href="./css/index.css" -> href="./css/index.css?v=25"
+    new_content = re.sub(r'\.css(["\'])', r'.css?v=25\1', content)
     
-    # Also bump existing ?v=X to ?v=24
-    new_content = re.sub(r'\.css\?v=\d+(["\'])', r'.css?v=24\1', new_content)
+    # Also bump existing ?v=X to ?v=25
+    new_content = re.sub(r'\.css\?v=\d+(["\'])', r'.css?v=25\1', new_content)
 
     if new_content != content:
         with open(file, 'w', encoding='utf-8') as f:
